9d6e70457966c771d47f5e3ecde2941e55d59f3e
Shane Cancilla <shanecancilla@gmail.com>
2023-12-06 11:52:59 -0800

    Added some stylings to recipe.css

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,47 +1,45 @@
-// JavaScript file for Recipe page (recipe.js)
-
 document.addEventListener('DOMContentLoaded', function() {
-  const recipeDetailsContainer = document.getElementById('recipeDetails');
+  const recipeDetailsElement = document.getElementById('recipeDetails');
 
   // Get the recipe ID from the URL
   const urlParams = new URLSearchParams(window.location.search);
   const recipeId = urlParams.get('id');
 
-  // Fetch details of the specific recipe based on the ID
   fetch(`/recipe/${recipeId}`)
     .then(response => response.json())
     .then(data => {
       const recipe = data.recipe;
 
-      // Display the recipe details
-      const titleElement = document.createElement('h2');
-      titleElement.textContent = recipe.title;
+      if (recipe) {
+        // Display the recipe details
+        const titleElement = document.createElement('h2');
+        titleElement.textContent = recipe.title;
 
-      const photoElement = document.createElement('img');
-      photoElement.src = recipe.photo;
+        const photoElement = document.createElement('img');
+        photoElement.src = recipe.photo;
 
-      const mealTypeElement = document.createElement('p');
-      mealTypeElement.textContent = recipe.mealType;
+        const mealTypeElement = document.createElement('p');
+        mealTypeElement.textContent = `Meal Type: ${recipe.mealType}`;
 
+        const ingredientsElement = document.createElement('p');
+        ingredientsElement.textContent = recipe.ingredients;
 
-      const ingredientsElement = document.createElement('p');
-      ingredientsElement.textContent = recipe.ingredients;
+        const instructionsElement = document.createElement('p');
+        instructionsElement.textContent = recipe.instructions;
 
-      const instructionsElement = document.createElement('p');
-      instructionsElement.textContent = recipe.instructions;
-
-      // Add other elements as needed to display recipe details
-
-      recipeDetailsContainer.appendChild(titleElement);
-      recipeDetailsContainer.appendChild(photoElement);
-      recipeDetailsContainer.appendChild(mealTypeElement);
-      recipeDetailsContainer.appendChild(ingredientsElement);
-      recipeDetailsContainer.appendChild(instructionsElement);
-      
-      // Append other recipe details as required
+        recipeDetailsElement.appendChild(titleElement);
+        recipeDetailsElement.appendChild(photoElement);
+        recipeDetailsElement.appendChild(mealTypeElement);
+        recipeDetailsElement.appendChild(ingredientsElement);
+        recipeDetailsElement.appendChild(instructionsElement);
+      } else {
+        const errorElement = document.createElement('p');
+        errorElement.textContent = 'Recipe not found.';
+        recipeDetailsElement.appendChild(errorElement);
+      }
     })
     .catch(error => {
-      console.error('Error fetching recipe details:', error);
+      console.error('Error fetching recipe:', error);
       // Handle errors, display error message, etc.
     });
 });
